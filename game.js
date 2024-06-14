349b288d1166d46363fc529ce68d7ae078ffa293
Jackson <jacksonli_@outlook.com>
2024-06-15 00:05:43 +1000

    Format button appearance

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-console.log("Welcome to a simple game of rock, paper, scissors!")
-
 function getComputerChoice(){
     let randInt = Math.random();
     if (randInt <= 0.33){
@@ -8,23 +6,6 @@ function getComputerChoice(){
         return "paper"
     } else {
         return "scissors";
-    }
-}
-
-function getHumanChoice(){
-    let userChoice = prompt("Rock, paper or scissors?");
-    let lowerUserChoice = userChoice.toLowerCase();
-
-
-    if (lowerUserChoice == "rock"){
-        return "rock";
-    } else if ((lowerUserChoice == "paper")) {
-        return "paper";
-    } else if ((lowerUserChoice == "scissors")) {
-        return "scissors"
-    } else {
-        alert("Invalid choice! Choose either rock, paper or scissors");
-        return getHumanChoice();
     }
 }
 
@@ -46,22 +27,6 @@ function playGame(rounds){
     let humanScore = 0;
     let computerScore = 0;
 
-    for (i=0; i < rounds; i++){
-
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        const res = playRound(humanSelection, computerSelection)
-
-        if (res == "human"){
-            console.log("Human using ".concat(humanSelection).concat(" defeated the computer who used ").concat(computerSelection));
-            humanScore++;
-        } else if (res == "computer"){
-            console.log("Computer using ".concat(computerSelection).concat(" defeated the human who used ").concat(humanSelection));
-            computerScore++;
-        } else {
-            console.log("This round was a draw!")
-        }
-    }
 
     if (humanScore == computerScore){
         return "We have a draw! Both players won " + humanScore + " times";
@@ -75,6 +40,9 @@ function playGame(rounds){
 // buttons is a node list. It looks and acts much like an array.
 const buttons = document.querySelectorAll("button");
 
+const humanScoreT = 0;
+const computerScoreT = 0;
+
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
   // and for each one we add a 'click' listener
@@ -82,6 +50,16 @@ buttons.forEach((button) => {
 
     const res = playRound(button.id.toLowerCase(), getComputerChoice())
 
-    alert(res);
+    if (res == "human"){
+        alert("Human won selecting " + button.id);
+        humanScore++;
+    } else if (res == "computer"){
+        alert("Human lost selecting " + button.id);
+        computerScore++;
+    } else {
+        alert("This round was a draw!")
+    }
   });
 });
+
+
