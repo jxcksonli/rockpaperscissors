0dbfda4ec95238733e1633f729df7cb7acf1917f
Jackson <jacksonli_@outlook.com>
2024-06-14 23:47:01 +1000

    Ensure a game can run when button is clicked

--- game.js
+++ game.js
@@ -72,4 +72,16 @@ function playGame(rounds){
     }
 }
 
-console.log(playGame(5))
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+
+    const res = playRound(button.id.toLowerCase(), getComputerChoice())
+
+    alert(res);
+  });
+});
